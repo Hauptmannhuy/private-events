5556ea7cf7e16a4ff41d0a95f0b71781a4393e4a
Hauptmannhuy <dindago2324@gmail.com>
2024-04-10 14:48:40 +0300

    form for creating an event

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_09_123458) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_10_113117) do
   create_table "events", force: :cascade do |t|
     t.integer "creator_id"
     t.string "location"
     t.datetime "date"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
