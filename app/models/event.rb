22240ff51f03844fd28780da02771e27937a35ea
Hauptmannhuy <dindago2324@gmail.com>
2024-04-10 16:09:03 +0300

    create view for event#show

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
-
+  validates :date, presence: true
+  validates :location, presence: true, length: {minimum: 5, maximum:15}
+  validates :description, length: {minimum: 10}
   belongs_to :creator, class_name: "User"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_10_113117) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_10_125658) do
   create_table "events", force: :cascade do |t|
     t.integer "creator_id"
     t.string "location"
     t.datetime "date"
-    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
