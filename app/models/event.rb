d75b7559f49d1e638a328004d2692d8af9a0388c
Hauptmannhuy <dindago2324@gmail.com>
2024-04-16 15:03:13 +0300

    added list of attendees in event show

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   validates :name, presence: true
   validates :date, presence: true
-  validates :location, presence: true, length: {minimum: 5, maximum:15}
+  validates :location, presence: true, length: {minimum: 2, maximum:15}
   validates :description, length: {minimum: 10}
   belongs_to :creator, class_name: "User"
   has_many :attendances, foreign_key: :attended_event_id
