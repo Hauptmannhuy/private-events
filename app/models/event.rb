0d9edc1bf4cc4736332fe2f84980c803e8a72ac8
Hauptmannhuy <dindago2324@gmail.com>
2024-04-16 19:03:15 +0300

    replaced class methods to separate events by date into scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,11 +7,14 @@ class Event < ApplicationRecord
   has_many :attendances, foreign_key: :attended_event_id
   has_many :attendees, through: :attendances, source: :attendee
 
-  def self.past
-    self.all.where('date < :date_today',date_today: Date.today)
-  end
+  # def self.past
+  #   self.all.where('date < :date_today',date_today: Date.today)
+  # end
 
-  def self.upcoming
-    self.all.where('date > :date_today',date_today: Date.today)
-  end
+  # def self.upcoming
+  #   self.all.where('date > :date_today',date_today: Date.today)
+  # end
+
+  scope :past, -> {where('date < :date_today',date_today: Date.today)}
+  scope :upcoming, -> {where('date > :date_today',date_today: Date.today)}
 end
